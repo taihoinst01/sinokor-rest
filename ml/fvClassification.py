65ff1c12e6dc3666c3d5130615c630cd0b82125c
JeongHanYoung <jhy2089@naver.com>
2018-07-06 10:04:00 +0900

    ml test 모듈 변경

--- ml/fvClassification.py
+++ ml/fvClassification.py
@@ -16,8 +16,8 @@ data = {
 
 body = str.encode(json.dumps(data))
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/40d09a0901e34fd1a176fb9af1e349a5/services/edcabd4c82cb400ca513c18455914623/execute?api-version=2.0&format=swagger'
-api_key = 'rQYcXzD8Q3E1PTohBeWqKN/eVw/aj7fKrflWjMVGMH6UEB1ZCEEVSYgNylf9Go6+HOUVrGOCdovDH+wvgvI3JA=='
+url = 'https://ussouthcentral.services.azureml.net/workspaces/40d09a0901e34fd1a176fb9af1e349a5/services/d6f5d5a684cf4ef9baebd421f1948ca7/execute?api-version=2.0&format=swagger'
+api_key = 'NhhxvPxkI1caxC76KiuZPKJXV3uZiB2oeFCpr7d6TmB+5Pc1yaLZrzVc64nuGKLQ94g0bLf99/jg7WotxXVfLQ=='
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
 req = urllib.request.Request(url, body, headers)
