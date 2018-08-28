eba9665ce1f20f111b07d35c8831323ba18959a7
JeongHanYoung <jhy2089@naver.com>
2018-08-28 13:25:52 +0900

    머신러닝 로직 추가

---
+++ ml/formMapping.py
@@ -0,0 +1,32 @@
+import urllib.request
+import json
+import sys
+
+data = {
+        "Inputs": {
+                "input1": json.loads(sys.argv[1]),
+        },
+    "GlobalParameters":  {
+    }
+}
+
+body = str.encode(json.dumps(data))
+
+url = 'https://ussouthcentral.services.azureml.net/workspaces/df3c2edb78144329b9b5e02a6b6b39ff/services/4022a75ffe1d472499a5e57866f961be/execute?api-version=2.0&format=swagger'
+api_key = 'qv1AlbUUU+yc9O4X1Y3xg8UoD8P2O91Qk7Zfm+pT9fZ0iSAcoVnu8vOa9OOZIztgmFaiF2rJBjW/6/2kMLu4eQ=='
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(json.dumps(result.decode("utf8", 'ignore')))
+
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(json.loads(error.read().decode("utf8", 'ignore')))
--- routes/ml.js
+++ routes/ml.js
@@ -12,28 +12,104 @@ router.get('/favicon.ico', function (req, res) {
 
 router.post('/api', function (req, res) {
     commMoudle.addLogging(req, JSON.stringify(req.body.data));
-    var fvParams = req.body.data;
-
-    // 고정가변 분류 알고리즘
-    var options = {
+    var data = req.body.data.data;
+	var type = req.body.type;
+	var param = [];
+	var targetPy = '';
+	
+	if(type == 'formLabelMapping'){
+		targetPy = 'formLabelMapping.py';
+        for (var i in data) {
+            param.push({ DATA: "'" + data[i].sid.replace(/,/g,"','") + "'", CLASS: 0 });
+		}
+	}else if(type == 'formMapping'){
+		if(!req.body.data.docCategory){
+			targetPy = 'formMapping.py';
+			var ogCompany = [];
+			var ctnm = [];
+			for(var i in data){
+				if(data[i].formLabel == 0){
+					ogCompany.push(data[i].text);
+				}else if(data[i].formLabel == 1){
+					ctnm.push(data[i].text);
+				}
+			}
+		}else{
+			res.send({ code: 200, data: req.body.data, message: 'exist form' });
+		}
+		
+		if(ogCompany.length == 1 && ctnm.length == 1){
+            param.push({ DATA: "'" + ogCompany[0].replace(/,/g, "','") + "','" + ctnm[0].replace(/,/g, "','") + "'", CLASS: 0});
+		}else if(ogCompany.length > 1 && ctnm.length == 1){
+			for(var i in ogCompany){
+                param.push({ DATA: "'" + ogCompany[i].replace(/,/g, "','") + "','" + ctnm[0].replace(/,/g, "','") + "'", CLASS: 0});
+			}
+		}else if(ogCompany.length == 1 && ctnm.length > 1){
+			for(var i in ctnm){
+                param.push({ DATA: "'" + ogCompany[0].replace(/,/g, "','") + "','" + ctnm[i].replace(/,/g, "','") + "'", CLASS: 0});
+			}
+		}
+		param.push({DATA: data[i].sid, CLASS: 0});
+	}else if(type == 'columnMapping'){
+		for(var i in data){
+            param.push({ DATA: "'" + req.body.data.docCategory.DOCTYPE + "','" + data[i].sid.replace(/,/g, "','") + "'", CLASS: 0});
+		}
+	}
+    /*var options = {
         mode: 'json',
         encoding: 'utf8',
         pythonPath: 'D:\\home\\python354x64\\python.exe',
         pythonOptions: ['-u'],
         scriptPath: require('app-root-path').path + '\\ml',
-        args: fvParams
+        args: [data]
+    };*/
+	
+	var options = {
+        mode: 'json',
+        encoding: 'utf8',
+        pythonPath: '',
+        pythonOptions: ['-u'],
+        scriptPath: require('app-root-path').path + '\\ml',
+        args: [JSON.stringify(param)]
     };
 
-    pythonShell.run('fvClassification.py', options, function (err, results) {
+    pythonShell.run(targetPy, options, function (err, results) {
         if (err) {
+			console.log(err);
             res.send({ code: 500, message: err });
         } else {
             results[0] = results[0].replace(/Scored Labels/gi, 'ScoredLabels');
             results[0] = results[0].replace(/Scored Probabilities/gi, 'ScoredProbabilities');
-            var fvResult = JSON.parse(results[0]).Results.output1;
-
-            commMoudle.addLogging(req, 'fixed or variable Classification Machine Learning Query Success.');
-            res.send({ code: 200, result: fvResult });
+            var outResult = JSON.parse(results[0]).Results.output1;
+			
+			if(type == 'formLabelMapping'){
+                for (var i in outResult) {
+                    if (req.body.data.data[i].sid == outResult[i].DATA.replace(/'/g,'')) {
+						if(!req.body.data.data[i].formLabel){
+							req.body.data.data[i].formLabel = outResult[i].ScoredLabels;
+						}
+					}			
+				}
+			}else if(type == 'formMapping'){
+				var docNum = 0;
+				var docScore = 0;
+				for(var i in outResult){
+					if(outResult[i]['ScoredProbabilities for Class "'+outResult[i].ScoredLabels+'"'] > docScore){
+						docNum = outResult[i].ScoredLabels;
+					}
+				}
+				req.body.data.docCategory = { DOCTYPE: docNum, Score: docScore };
+			}else if(type == 'columnMapping'){
+				for(var i in outResult){
+                    if (req.body.data.docCategory.DOCTYPE + ',' + req.body.data.data[i].sid == outResult[i].DATA.replace(/'/g, '')){
+						if(!req.body.data.data[i].colLbl){
+							req.body.data.data[i].colLbl = outResult[i].ScoredLabels;
+						}
+					}			
+				}
+			}
+            commMoudle.addLogging(req, type + ' Machine Learning Query Success.');
+            res.send(req.body.data);
         }
     });
 });
